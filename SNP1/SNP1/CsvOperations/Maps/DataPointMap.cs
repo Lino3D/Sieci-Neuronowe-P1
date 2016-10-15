125addb74f0ffc9e568d59eefbedb263dcb85f41
Lino3D <kamiskijakub@gmail.com>
2016-10-15 16:10:24 +0200

    Added Map for Datapoint

--- SNP1/SNP1/CsvOperations/Maps/DataPointMap.cs
+++ SNP1/SNP1/CsvOperations/Maps/DataPointMap.cs
@@ -4,13 +4,12 @@ using System.Globalization;
 
 namespace SNP1.CsvOperations.Maps
 {
-    internal class DataPointClsMap : CsvClassMap<DataPointCls>
+    public class DataPointMap : CsvClassMap<DataPoint>
     {
-        public DataPointClsMap()
+        public DataPointMap()
         {
             Map(m => m.X).Index(0).TypeConverterOption(CultureInfo.InvariantCulture);
             Map(m => m.Y).Index(1).TypeConverterOption(CultureInfo.InvariantCulture);
-            Map(m => m.Cls).Index(2);
         }
     }
 }
