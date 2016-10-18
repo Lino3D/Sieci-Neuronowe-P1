7df52ed139431ed4b5e39105f7fc9954ba7928f6
Michał Grabowski <michal.grabowski@wum.edu.pl>
2016-10-18 17:28:21 +0200

    W wersji V2 podpiąłem framework caliburn.micro

---
+++ SNP1/WinAppV2/AppBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAppV2
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+
+
+}
